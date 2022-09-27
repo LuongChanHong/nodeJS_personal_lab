8e3a1934d013e46bddcc7d7f1236c1ed8ff82ef3
honglc <hongmainmail@gmail.com>
2022-09-27 15:43:32 +0700

    render product detail by url param

--- routes/shop.js
+++ routes/shop.js
@@ -16,4 +16,6 @@ router.get("/orders", shopController.getOrders);
 
 router.get("/checkout", shopController.getCheckout);
 
+router.get("/products/:productID", shopController.getProduct);
+
 module.exports = router;
