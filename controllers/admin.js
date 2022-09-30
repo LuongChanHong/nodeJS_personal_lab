329cc4679362db3e596a2ac8db56fe1470b23fd4
honglc <hongmainmail@gmail.com>
2022-09-30 17:32:13 +0700

    lab8.2

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,14 @@
-const Product = require('../models/product');
+const { response } = require("express");
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
+    // formsCSS: true,
+    // productCSS: true,
+    // activeAddProduct: true,
   });
 };
 
@@ -17,15 +19,35 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const product = new Product(title, imageUrl, description, price);
   product.save();
-  res.redirect('/');
+  res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  //query param set optional information
+  const editMode = req.query.edit;
+  const productID = req.params.productID;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  Product.findById(productID, (product) => {
+    if (!product) {
+      return redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
+  });
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
       prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+      pageTitle: "Admin Products",
+      path: "/admin/products",
     });
   });
 };
